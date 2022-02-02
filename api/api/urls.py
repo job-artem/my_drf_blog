e4ffcce6c4990b472a6ab8fb2b99451edc04820b
artem-cyberpolice <artem.puhach@cyberpolice.gov.ua>
2022-02-02 13:04:26 +0200

    https://stepik.org/lesson/450478/step/4?unit=440919

--- api/api/urls.py
+++ api/api/urls.py
@@ -21,8 +21,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
     path("api/token/", TokenObtainPairView.as_view(), name="token"),
     path("api/refresh_token/", TokenRefreshView.as_view(), name="refresh_token"),
     path("ckeditor/", include('ckeditor_uploader.urls')),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- api/core/admin.py
+++ api/core/admin.py
@@ -3,7 +3,14 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'tag_list')
     pass
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
 
 
 admin.site.register(Post, PostAdmin)
