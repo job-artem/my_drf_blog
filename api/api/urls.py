928fcd142b9cd11829c66c114e65499225e811c5
artem-cyberpolice <artem.puhach@cyberpolice.gov.ua>
2022-02-02 10:20:53 +0200

    https://stepik.org/lesson/450478/step/1?unit=440919

--- api/api/urls.py
+++ api/api/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("api/token/", TokenObtainPairView.as_view(), name="token"),
+    path("api/refresh_token/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("ckeditor/", include('ckeditor_uploader.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
